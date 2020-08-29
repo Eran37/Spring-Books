d43a8f2a362e497ce8b27ef6f4bccd5c6f41d7c1
Eran37 <doc.assraf@gmail.com>
2020-08-29 21:08:09 +0300

    SpringBooksDone

--- src/main/java/pack/controllers/AuthorController.java
+++ src/main/java/pack/controllers/AuthorController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pack.entities.Author;
 import pack.service.AuthorService;
 
@@ -26,7 +23,6 @@ public class AuthorController {
         this.service = service;
     }
 
-    //TODO - To fix the recursive problem of author/byId
     //  http://localhost:7070/author/byId/1
     @GetMapping("/byId/{id}")
     public ResponseEntity getById(@PathVariable("id") Long id) {
@@ -42,6 +38,52 @@ public class AuthorController {
                         .status(HttpStatus.OK)
                         .contentType(MediaType.TEXT_PLAIN)
                         .body("Author by the id " + id + " does not exist");
+    }
+
+    //  http://localhost:7070/author/create
+    @PostMapping("/create")
+    public ResponseEntity create (@RequestBody Author author) {
+        try {
+            Author afterSave = service.save(author);
+
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(afterSave);
+
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .contentType(MediaType.TEXT_PLAIN)
+                    .body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity update(@RequestBody Author author) {
+        if (author.getId() == null) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .contentType(MediaType.TEXT_PLAIN)
+                    .body("In order to update must provide id");
+        }
+        return create(author);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity delete(@PathVariable("id") Long id) {
+        try {
+            Author deleted = service.deleteById(id);
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(deleted);
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .contentType(MediaType.TEXT_PLAIN)
+                    .body(e.getMessage());
+        }
     }
 
 }
